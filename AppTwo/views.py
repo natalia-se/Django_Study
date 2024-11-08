2e85ce76da9cccaeda57d25255c2b045bcf7b22b
Nataliia Ishchenko <nataliia.ishchenko@desifer.se>
2024-11-08 14:33:35 +0100

    First Django project

---
+++ AppTwo/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'AppTwo/home.html')
+
+def help(request):
+    return render(request, 'AppTwo/help.html')
